dd3c34bc55f12c2e1b8fa1498a3f99dcd4d2695c
Javier Sanhueza <jsanhueza@ast.cl>
2026-02-05 09:50:50 -0300

    ïmplementacion logica multiples conexiones

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from routes.Radar import radar_listener_task
+import asyncio
 
-
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Iniciando servidor: Conectando con la tarea del radar...")
+    
+    radar_task = asyncio.create_task(radar_listener_task())
+    
+    yield
+    
+    print("Apagando servidor: Cancelando tarea del radar...")
+    radar_task.cancel()
+    try:
+        await radar_task
+    except asyncio.CancelledError:
+        print("Tarea del radar cancelada correctamente.")
+        
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
